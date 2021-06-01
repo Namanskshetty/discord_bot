ffb5ae2a39dd8c21d6786a8c0011174018cd117e
Namanskshetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-01 10:27:59 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "starting bash"
+echo -e "\e[1;31m Installing new version of pip \e[0m"
+pip install --upgrade pip
+echo -e "\e[1;31m Installing requirents \e[0m"
 pip install -r requirements.txt
+echo "Wait for 5 seconnds"
 sleep 5
 python3 main.py
 
