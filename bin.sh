6af00128ba2fb5ef3b947558061d69eb96a45714
Naman Shetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-11 21:28:36 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-figlet "WELCOME"
+eho " _    _  ____  __    ___  _____  __  __  ____ 
+( \/\/ )( ___)(  )  / __)(  _  )(  \/  )( ___)
+ )    (  )__)  )(__( (__  )(_)(  )    (  )__) 
+(__/\__)(____)(____)\___)(_____)(_/\/\_)(____)"
 echo "starting bash"
 echo -e "\e[1;31m Installing new version of pip and Installing requirents \e[0m"
 chmod +x script.sh
